4c743cfe504315982d37c6920871d07cfeda3888
Dimitri Ferguson <dimitri.ferguson93@gmail.com>
2022-06-06 13:19:08 -0400

    Adding styles to REST-rant

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,13 @@ function Def(html) {
     <html>
       <head>
         <title>Title</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+          integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+          crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="/CSS/style.css" />
       </head>
       <body>{html.children}</body>
     </html>
