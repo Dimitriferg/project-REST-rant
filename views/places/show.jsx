28bdc0b84179589ba45f6141796f05d069c08c92
Dimitri Ferguson <dimitri.ferguson93@gmail.com>
2022-06-17 15:41:20 -0400

    Created helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,15 +6,34 @@ function show(data) {
     <Def>
       <main>
         <h1>{data.place.name}</h1>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+        <h2>Rating</h2>
+        <p>Currently Unrated</p>
+        <h2>Description</h2>
+        <h3>{data.place.showEstablished()}</h3>
+        <div>
+          <img src={data.place.pic} alt={data.place.name}></img>
+        </div>
+        {/* Edit button */}
+        <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
           Edit
         </a>
-
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+        {/* Delete Button */}
+        <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
           <button type="submit" className="btn btn-danger">
             Delete
           </button>
         </form>
+        <h3>Rating</h3>
+        {rating}
+        <hr></hr>
+        <h2>Comments</h2>
+        {comments}
+        <a
+          href={`/places/${data.place.id}/comment`}
+          className="btn btn-warning"
+        >
+          Add Comment
+        </a>
       </main>
     </Def>
   );
